5a40181a1742952ba3bd9d4068a748871e861a37
kirislmc <kirislmc@icloud.com>
2025-08-16 20:35:34 +0200

    Add store list
    Added a  store list with prices if item is in store.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-instore_items = ["chicken", "shawarma", "fries"]
+item_in_store = {"shawarma": 4.99,
+                  "chicken": 8.99,
+                  "salad": 3.99,
+                  "sauce": 1.4}
+
 
 print("Welcome to the Skibidi Shop!")
 
-item = input("Which item would you like to buy?: ")
+item = input("Which item would you like to buy?: ").lower()
+if item in item_in_store:
+    price = item_in_store.get(item)
+    amount = int(input(f"How much of {item.capitalize()} do you want?: "))
 
-if item in instore_items:
-    price = float(input("How much does it cost?: "))
-    amount = int(input(f"How much of {item} do you want?: "))
     end_price = price * amount
-    print(f"You have put now in your cart {amount}x {item}")
-    print(f"Your price will be {end_price}€")
+
+    print(f"You have put now in your cart {amount}x {item.capitalize()}")
+    print(f"Your price will be {end_price:.2f}€")
     print("Thank you for shopping with us!")
 else:
-    print(f"We do not have {item}. Please try again.")
+    print(f"We do not have {item.capitalize()} please choose our items in store.")
+
+
+
