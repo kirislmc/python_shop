b09afdc688058a9531450b226352991886c3e18e
ashis <161737873+axhis8@users.noreply.github.com>
2025-08-16 19:08:55 +0200

    Add in store list
    Added a list to check if the item is available in store, if not it prints that it is not availible. So the user cant buy anything that doesn't make sense.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+instore_items = ["chicken", "shawarma", "fries"]
+
 print("Welcome to the Skibidi Shop!")
 
 item = input("Which item would you like to buy?: ")
-price = float(input("How much does it cost?: "))
-amount = int(input(f"How much of {item} do you want?: "))
 
-end_price = price * amount
-
-
-print(f"You have put now in your cart {amount}x {item}")
-print(f"Your price will be {end_price}€")
-print("Thank you for shopping with us!")
-
-
-
+if item in instore_items:
+    price = float(input("How much does it cost?: "))
+    amount = int(input(f"How much of {item} do you want?: "))
+    end_price = price * amount
+    print(f"You have put now in your cart {amount}x {item}")
+    print(f"Your price will be {end_price}€")
+    print("Thank you for shopping with us!")
+else:
+    print(f"We do not have {item}. Please try again.")
